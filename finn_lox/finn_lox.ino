a3130b31b7077ccd39b36585e854015bd019b717
Michael Chugunov <Michael Chugunov>
2017-02-18 14:46:42 +0300

    More Changes

--- finn_lox/finn_lox.ino
+++ finn_lox/finn_lox.ino
@@ -2,6 +2,7 @@
 
 #define DHTPIN0 2 // номер пина, к которому подсоединен датчик
 #define DHTPIN1 3 // номер пина, к которому подсоединен датчик
+/*
 #define DHTPIN2 4 // номер пина, к которому подсоединен датчик
 #define DHTPIN3 5 // номер пина, к которому подсоединен датчик
 #define DHTPIN4 6 // номер пина, к которому подсоединен датчик
@@ -12,6 +13,7 @@
 #define DHTPIN9 11 // номер пина, к которому подсоединен датчик
 #define DHTPIN10 12 // номер пина, к которому подсоединен датчик
 #define DHTPIN11 13 // номер пина, к которому подсоединен датчик
+*/
 
 
 // Раскомментируйте в соответствии с используемым датчиком
@@ -19,6 +21,7 @@
 
 DHT dht0(DHTPIN0, DHT22);
 DHT dht1(DHTPIN1, DHT22);
+/*
 DHT dht2(DHTPIN2, DHT22);
 DHT dht3(DHTPIN3, DHT22);
 DHT dht4(DHTPIN4, DHT22);
@@ -29,6 +32,7 @@ DHT dht8(DHTPIN8, DHT22);
 DHT dht9(DHTPIN9, DHT22);
 DHT dht10(DHTPIN10, DHT22);
 DHT dht11(DHTPIN11, DHT22);
+*/
 
 
 //DHT dht(DHTPIN1, DHT11);
@@ -36,23 +40,25 @@ DHT dht11(DHTPIN11, DHT22);
 void setup() {
     Serial.begin(9600);
     dht0.begin();
+    dht1.begin();
 }
 
 //наплодить еще запросов
 
 void loop() {
-    float data_f[0][0];
+    char data_f[8][8];
     // Задержка 2 секунды между измерениями
     delay(2000);
     //Считываем влажность
-    float humidity = dht0.readHumidity();
-    // Считываем температуру
-    float temperature = dht0.readTemperature();
-    // Проверка удачно прошло ли считывание.
-    if (isnan(humidity) || isnan(temperature)) {
-        Serial.println("Не удается считать показания");
-    return;
+    data_f[0][0] = {(char)dht0.readHumidity() + (char)dht0.readTemperature()};
+    data_f[0][1] = {(char)dht1.readHumidity() + (char)dht1.readTemperature()};
+    for(int i = 1; i <= (sizeof(data_f)/sizeof(float)); i++){
+      for(int j = 1; j <= (sizeof(data_f[i])/sizeof(float)); i++){
+        
+      }
     }
-
-    Serial.print("jhd");
+/*
+    Serial.println(temperature);
+    Serial.println(temperature1);
+    */
 }
